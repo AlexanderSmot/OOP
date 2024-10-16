76fb4c73831435157dd186492e535585ec222713
Alex <virus2005@yandex.ru>
2024-10-16 16:50:43 +0400

    modified OOP

--- OOP.py
+++ OOP.py
@@ -58,7 +58,9 @@ class Mentor:
         self.courses_attached = []
 
 class Lecturer(Mentor):
-    grades = {}
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.grades = {}
 
     def avg_rate(self):
         sum_ = 0
@@ -73,10 +75,22 @@ class Lecturer(Mentor):
         return f' Имя: {self.name} \n Фамилия: {self.surname} \n Средняя оценка за лекции: {self.avg_rate()}'
 
     def __lt__(self, other):
-        if not isinstance(other, Student):
+        if not isinstance(other, Lecturer):
             print("Разные категории людей не сравниваем.")
             return
-        return self.st_avg_rate() < other.st_avg_rate()
+        return self.avg_rate() < other.avg_rate()
+
+    def __gt__(self, other):
+        if not isinstance(other, Lecturer):
+            print("Разные категории людей не сравниваем.")
+            return
+        return self.avg_rate() > other.avg_rate()
+
+    def __eq__(self, other):
+        if not isinstance(other, Lecturer):
+            print("Разные категории людей не сравниваем.")
+            return
+        return self.avg_rate() == other.avg_rate()
 
 class Reviewer(Mentor):
 
@@ -98,7 +112,7 @@ best_student_1.courses_in_progress += ['Python', 'Git']
 best_student_1.finished_courses += ['Введение в программирование']
 
 best_student_2 = Student('Maks', 'Min', 'your_gender')
-best_student_2.courses_in_progress += ['C++', 'Git']
+best_student_2.courses_in_progress += ['C++', 'Python']
 best_student_2.finished_courses += ['Введение в программирование']
 
 cool_reviewer_1 = Reviewer('Some', 'Buddy')
@@ -122,12 +136,16 @@ print(some_reviewer)
 cool_lecturer_1 = Lecturer('Some', 'Buddy')
 cool_lecturer_1.courses_attached += ['Python', 'C+']
 cool_lecturer_2 = Lecturer('Mr', 'Voice')
-cool_lecturer_2.courses_attached += ['Git', 'C+']
+cool_lecturer_2.courses_attached += ['Git', 'Python']
 
 
 best_student_1.lecturer_grade(cool_lecturer_1, 'Python', 8)
 best_student_1.lecturer_grade(cool_lecturer_1, 'Python', 7)
 best_student_1.lecturer_grade(cool_lecturer_1, 'Python', 10)
+
+best_student_1.lecturer_grade(cool_lecturer_2, 'Python', 10)
+best_student_1.lecturer_grade(cool_lecturer_2, 'Python', 10)
+best_student_1.lecturer_grade(cool_lecturer_2, 'Python', 10)
 
 print(best_student_1.grades)
 print(cool_lecturer_1.grades)
@@ -140,3 +158,54 @@ elif best_student_1 > best_student_2:
     print(best_student_1.name, ' has higher rate than', best_student_2.name)
 else:
     print(best_student_1.name, ' has the same rate as', best_student_2.name)
+
+if cool_lecturer_1 < cool_lecturer_2:
+    print(cool_lecturer_1.name, ' has less rate than', cool_lecturer_2.name)
+elif cool_lecturer_1 > cool_lecturer_2:
+    print(cool_lecturer_1.name, ' has higher rate than', cool_lecturer_2.name)
+else:
+    print(cool_lecturer_1.name, ' has the same rate as', cool_lecturer_2.name)
+
+# Cредняя оценка за домашние задания по всем студентам в рамках конкретного курса
+
+student_list = [best_student_1, best_student_2]
+
+
+def average_mark_student(student_list, course_name):
+    summary = 0
+    for student in student_list:
+        for course in student.grades.keys():
+            if course != course_name:
+                pass
+            else:
+                for mar in student.grades.values():
+                    summary += sum(mar)
+
+    res = summary / len(student_list)
+    return res
+
+
+ams = average_mark_student(student_list, 'Python')
+print(f' Средняя оценка за домашние задания по всем студентам {ams}')
+
+# Средняя оценка за лекции всех лекторов в рамках курса
+
+lecturer_list = [cool_lecturer_1, cool_lecturer_2]
+
+
+def average_mark_lecturer(lecturer_list, course_name):
+    summary = 0
+    for lecturer in lecturer_list:
+        for course in lecturer.grades.keys():
+            if course != course_name:
+                pass
+            else:
+                for mar in lecturer.grades.values():
+                    summary += sum(mar)
+
+    res = summary / len(lecturer_list)
+    return res
+
+
+aml = average_mark_lecturer(lecturer_list, 'Python')
+print(f' Средняя оценка за лекции по всем лекторам {aml}')
